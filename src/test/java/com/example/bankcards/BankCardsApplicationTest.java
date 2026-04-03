c944d8a298f02801155f092201d48b0f2690099c
Ivan Yarigo <yarigo2018@yarigo.ru>
2026-04-03 14:34:39 +0300

    main context test

---
+++ src/test/java/com/example/bankcards/BankCardsApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.bankcards;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class BankCardsApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
